e4940ffa171ab1a5a440e9c75a5c81238093403e
Ericg411 <ericg411@gmail.com>
2025-05-29 17:39:34 -0500

    Made algorithm fast enough to pass the leetcode tests

--- solution.py
+++ solution.py
@@ -6,18 +6,21 @@ class Solution:
 
         def backtrace(target, current, start):
             if target == 0:
-                current.sort()
                 if current not in result:
                     result.append(current)
                 return
             else:
                 for i in range(start, len(candidates)):
-                    if candidates[i] > target:
+                    if i > start and candidates[i] == candidates[i - 1]:
                         continue
-                    else:
-                        current.append(candidates[i])
-                        backtrace(target - candidates[i], current[:], i + 1)
-                        current.pop()
+                    
+                    if candidates[i] > target:
+                        break
+                   
+                    current.append(candidates[i])
+                    backtrace(target - candidates[i], current[:], i + 1)
+                    current.pop()
 
+        candidates.sort()
         backtrace(target, [], 0)
         return result
